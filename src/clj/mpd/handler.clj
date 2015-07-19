e932723f6f9097132d60b76c6d43f8eaa5d95b00
keymone <keymone@gmail.com>
2015-07-19 13:31:07 +0200

    serve index.html by default

--- src/clj/mpd/handler.clj
+++ src/clj/mpd/handler.clj
@@ -6,12 +6,19 @@
         ring.middleware.file-info)
   (:require [org.httpkit.server :as http-kit]))
 
+(defn wrap-dir-index [handler]
+  (fn [req]
+    (handler
+      (update-in req [:uri]
+                 #(if (= "/" %) "/index.html" %)))))
+
 (def app
   (-> (fn [req] {:status 404
                  :headers {"Content-Type" "text/html"}
                  :body "Gone"})
       (wrap-resource "public")
       (wrap-file-info)
+      (wrap-dir-index)
       (wrap-with-logger)
       (wrap-reload)))
 
