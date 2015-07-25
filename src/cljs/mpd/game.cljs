4a337a97a3d2f992608812362f8a50c72b9ced67
Maksym Melnychok <maksym@yelp.com>
2015-07-25 18:33:57 +0200

    move rendering state into anonymous closure

--- src/cljs/mpd/game.cljs
+++ src/cljs/mpd/game.cljs
@@ -2,4 +2,9 @@
   (:require [mpd.shared :refer [log]]))
 
 (defn setup [stage player network]
-  (log "  game"))
+  (log "  game")
+  (main-loop stage player network))
+
+(defn main-loop [stage player network]
+  (stage)
+  (js/requestAnimationFrame main-loop))
--- src/cljs/mpd/stage.cljs
+++ src/cljs/mpd/stage.cljs
@@ -2,31 +2,36 @@
   (:require [mpd.shared :refer [log]]
             [mpd.stage.map :as map]))
 
-(def renderer (js/PIXI.autoDetectRenderer
-                (.-innerWidth js/window)
-                (.-innerHeight js/window)
-                {:antialiasing false
-                 :transparent false
-                 :resolution 1}))
-(set! (.-backgroundColor renderer) "0xFFFFFF")
-(.appendChild (.-body js/document) (.-view renderer))
-
-(def stats (js/Stats.))
-(set! (-> (.-domElement stats) .-style .-position) "absolute")
-(set! (-> (.-domElement stats) .-style .-display) "inline-block")
-(set! (-> (.-domElement stats) .-style .-top) "0px")
-(set! (-> (.-domElement stats) .-style .-right) "0px")
-(.appendChild (.-body js/document) (.-domElement stats))
-
-(def stage (js/PIXI.Container.))
-(.addChild stage map/container)
-
-(defn render-stage []
-  (js/requestAnimationFrame render-stage)
-  (.begin stats)
-  (.render renderer stage)
-  (.end stats))
+; state - hierarchical primitive-only representation
+; of current frame:
+;
+; [ [ <entity> <params> [ <children> ] ],
+;   [ ... ] ]
+;
+(defn state-to-pixi [state]
+  state)
 
 (defn setup []
-  (js/requestAnimationFrame render-stage)
-  (log "  stage"))
+  (log "  stage")
+  (let [renderer (js/PIXI.autoDetectRenderer
+                   (.-innerWidth js/window)
+                   (.-innerHeight js/window)
+                   {:antialiasing false
+                    :transparent false
+                    :resolution 1})
+        stats (js/Stats.)
+        stage (js/PIXI.Container.)]
+    (set! (.-backgroundColor renderer) "0xFFFFFF")
+    (.appendChild (.-body js/document) (.-view renderer))
+
+    (set! (-> (.-domElement stats) .-style .-position) "absolute")
+    (set! (-> (.-domElement stats) .-style .-display) "inline-block")
+    (set! (-> (.-domElement stats) .-style .-top) "0px")
+    (set! (-> (.-domElement stats) .-style .-right) "0px")
+    (.appendChild (.-body js/document) (.-domElement stats))
+
+    (.addChild stage map/container)
+
+    #(do (.begin stats)
+         (.render renderer (state-to-pixi stage)) ; use % here to pass game state
+         (.end stats))))
