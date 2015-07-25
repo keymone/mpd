979b1ebb487d054106df1ba9afd8e67c98fd50c2
Maksym Melnychok <maksym@yelp.com>
2015-07-25 22:39:15 +0200

    key up/down on player

--- src/cljs/mpd/player.cljs
+++ src/cljs/mpd/player.cljs
@@ -1,6 +1,52 @@
 (ns mpd.player
   (:require [mpd.shared :refer [log]]))
 
+(def input (atom {:up false ; 38 87
+                  :down false; 40 83
+                  :left false; 37 65
+                  :right false; 39 68
+                  :click false
+                  :rclick false
+                  :mousex 0
+                  :mousey 0}))
+
+(defn keyhandler [event]
+  (let [key (case (.-keyCode event)
+              (38 87) :up
+              (40 83) :down
+              (37 65) :left
+              (39 68) :right
+              "default" nil)
+        updown (case (.-type event)
+                 "keydown" true
+                 "keyup" false
+                 "default" nil)]
+    (when (not (or (= key nil) (= updown nil)))
+      (when (not (.-repeat event))
+        (swap! input assoc key updown)))))
+
+(defn mousehandler [event]
+  )
+
+(defn movehandler [event]
+  )
+
+(defn start-listening []
+  (aset js/document "onkeydown"   keyhandler)
+  (aset js/document "onkeyup"     keyhandler)
+  (aset js/document "onmousedown" mousehandler)
+  (aset js/document "onmouseup"   mousehandler)
+  (aset js/document "onmousemove" movehandler))
+
 (defn setup [stage player network]
   (log "  player")
-  (fn [state] state))
+  (fn [state]
+    (start-listening)
+    (let [player (:player @state)
+          x (:x player)
+          y (:y player)
+          dx (if (:left @input) -1 (if (:right @input) 1 0))
+          dy (if (:up @input) -1 (if (:down @input) 1 0))]
+      (when (not (= dy 0)) (swap! state assoc-in [:player :y] (+ y dy)))
+      (when (not (= dx 0)) (swap! state assoc-in [:player :x] (+ x dx))))
+    state))
