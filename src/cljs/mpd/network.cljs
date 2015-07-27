5e834aacb5e1ae768b1288177e0e9b7344298ac1
Maciej Gajek <maltize@gmail.com>
2015-07-27 15:47:19 +0200

    network basic cleanup

--- src/cljs/mpd/network.cljs
+++ src/cljs/mpd/network.cljs
@@ -4,20 +4,28 @@
 (defn parseJSON [x]
   (.parse (.-JSON js/window) x))
 
-(def websocket* (atom []))
+(def websocket (js/WebSocket. "ws://localhost:8192"))
+
+(def inbox [])
+
+(defn clj->json
+  [ds]
+  (.stringify js/JSON (clj->js ds)))
 
 (defn- send [m]
-  (.send @websocket* m))
+  (.send @websocket m))
 
 (defn- receive [m]
-  (log (.-data m)))
+  (let [data (.-data m)]
+    (def inbox (conj inbox data))
+    (log data)))
 
 (defn setup []
   (log "  network")
   (log "connecting...")
-  (reset! websocket* (js/WebSocket. "ws://localhost:8192"))
+
   (doall
-      (map #(aset @websocket* (first %) (second %))
+      (map #(aset websocket (first %) (second %))
            [["onopen" (fn [] (log "OPEN"))]
             ["onclose" (fn [] (log "CLOSE"))]
             ["onerror" (fn [e] (log (str "ERROR:" e)))]
@@ -25,4 +33,8 @@
 
   (log "connected.")
 
-  (fn [state] state))
+  (fn [state]
+    ; erase inbox queue for now
+    (def inbox [])
+    (log " network update")
+    state))
