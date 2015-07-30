d3584ada6b71272e6e484015beb3e2090d4f55b1
Maksym Melnychok <maksym@yelp.com>
2015-07-30 21:30:27 +0200

    pixelperfection

--- src/cljs/mpd/player.cljs
+++ src/cljs/mpd/player.cljs
@@ -49,8 +49,8 @@
 ; FIRE
 
 (defn make-bullet [player]
-  {:x (+ (:x player) (* (Math/cos (:rotation player)) 80))
-   :y (+ (:y player) (* (Math/sin (:rotation player)) 80))
+  {:x (+ (:x player) (* (Math/cos (+ 0.17 (:rotation player))) 80))
+   :y (+ (:y player) (* (Math/sin (+ 0.17 (:rotation player))) 80))
    :angle (:rotation player)
    :speed 10
    :distance 100
